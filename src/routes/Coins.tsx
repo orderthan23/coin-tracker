27586eee88ef73a00dbad4bdb25e5821b62f0843
jonghyunKo <skygg@naver.com>
2021-12-20 20:06:38 +0900

    Route States

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -22,9 +22,10 @@ const Coin = styled.li`
     border-radius: 15px;
     margin-bottom : 10px;
     a{
+        display : flex;
+        align-items: center;
         padding : 20px;
         transition : color 0.2s ease-in;
-        display : block;
     }
     &:hover{
         a{
@@ -34,12 +35,19 @@ const Coin = styled.li`
 `;
 
 const Title = styled.h1`
+    font-size : 48px;
     color : ${props => props.theme.accentColor};
 `;
 
 const Loader = styled.span`
     text-align : center;
     display : block;
+`;
+
+const Img = styled.img`
+    width : 35px;
+    height : 35px;
+    margin-right : 10px;
 `;
 
 interface CoinInterface {
@@ -77,7 +85,12 @@ function Coins(){
           <CoinList>
             {coins.map((coin) => (
               <Coin key={coin.id}>
-                <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+                <Link to={{
+                    pathname : `/${coin.id}`,
+                    state : {name : coin.name}
+                }}>
+                    <Img src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}/>
+                    {coin.name} &rarr;</Link>
               </Coin>
             ))}
           </CoinList>
