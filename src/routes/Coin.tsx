603475ffe080f65145650fd7c591aaa1de4851b6
jonghyunKo <skygg@naver.com>
2021-12-20 21:24:46 +0900

    detail API Connect

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -32,24 +32,87 @@ interface RouteState{
     name: string;
 }
 
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    contract: string;
+    platform: string;
+    contracts: object;
+    parent: object;
+    tags: object;
+    team: object;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    links: object;
+    links_extended: object;
+    whitepaper: object;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    circulating_supply: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            ath_date: string
+            ath_price: number
+            market_cap: number
+            market_cap_change_24h: number
+            percent_change_1h: number
+            percent_change_1y: number
+            percent_change_6h: number
+            percent_change_7d: number
+            percent_change_12h: number
+            percent_change_15m: number
+            percent_change_24h: number
+            percent_change_30d: number
+            percent_change_30m: number
+            percent_from_price_ath: number
+            price: number
+            volume_24h: number
+            volume_24h_change_24h: number
+        }
+    };
+}
+
 function Coin(){
     const [loading, setLoading] = useState(true);
     const {coinId} = useParams<RouteParams>();
     const {state} = useLocation<RouteState>();
-    const [info, setInfo] = useState({});
-    const [priceInfo, setPriceInfo] = useState({});
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
 
     useEffect(()=>{
         (async() =>{
             const infoData = await(
                 await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
             ).json();
-            console.log("infoData", infoData);
 
             const priceData = await(
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
-            console.log("priceData", priceData);
                 setInfo(infoData);
                 setPriceInfo(priceData);
          })();
@@ -59,7 +122,7 @@ function Coin(){
         <Header>
           <Title>{state?.name || "Loading"}</Title>
         </Header>
-        {loading ? <Loader>Loading...</Loader> : <span>{info}</span>}
+        {loading ? <Loader>Loading...</Loader> : }
       </Container>
     );
     
