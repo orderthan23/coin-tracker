be74f7ca3feecd1a0361c58adc87417755a4827b
jonghyunKo <skygg@naver.com>
2021-12-21 11:11:43 +0900

    react-query1

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import { ThemeProvider } from 'styled-components';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {theme} from "./theme";
+import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <QueryClientProvider client={queryClient}>
+     <ThemeProvider theme={theme}>
     <App />
     </ThemeProvider>
+    </QueryClientProvider>
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "../api";
 
 
 
@@ -50,7 +52,7 @@ const Img = styled.img`
     margin-right : 10px;
 `;
 
-interface CoinInterface {
+interface ICoin {
   id: string;
   name: string;
   symbol: string;
@@ -58,32 +60,22 @@ interface CoinInterface {
   is_new: boolean;
   is_active: boolean;
   type: string;
-};
-function Coins(){
-    const [coins, setCoins] = useState<CoinInterface[]>([]);
-    const [loading, setLoading] = useState(true);
+}
 
-    useEffect(()=>{
-        (async() =>{
-           const response = await fetch('https://api.coinpaprika.com/v1/coins')
-           const json = await response.json();
-           setCoins(json.slice(0, 100));
-           setLoading(false);
-           console.log("coins", coins);
-        })();
-    },[]);
 
+function Coins() {
+  const {isLoading, data } =  useQuery<ICoin[]>("allCoins", fetchCoins)
     return (
       <Container>
         <Header>
           <Title>코인</Title>
         </Header>
 
-        {loading ? (
+        {isLoading ? (
           <Loader>Loading...</Loader>
         ) : (
           <CoinList>
-            {coins.map((coin) => (
+            {data?.slice(0, 100).map((coin) => (
               <Coin key={coin.id}>
                 <Link to={{
                     pathname : `/${coin.id}`,
